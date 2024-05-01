f7ff5628bb869b73ffaa7d4fd8aa8a6094762b26
LucBonnet <lucbonnet10@gmail.com>
2024-05-01 15:50:07 -0300

    camera travada

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,49 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField]private Transform target;
+    private Camera cam;
+    public SpriteRenderer cenario;
+    private float boundMinX, boundMaxX, boundMinY, boundMaxY;
+    private Boolean limit;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = GetComponent<Camera>();
+        boundMinX = cenario.bounds.min.x;
+        boundMaxX = cenario.bounds.max.x;
+        boundMinY = cenario.bounds.min.y;
+        boundMaxY = cenario.bounds.max.y;
+        limit = false;
     }
 
     // Update is called once per frame
     private void LateUpdate()
     {
-        transform.position = new Vector3(target.position.x,target.position.y,-20);
+        limit = false;
+        float halfHeight = cam.orthographicSize;
+        float halfWidth = cam.aspect * halfHeight;
+
+        Vector3 newPosition = target.position;
+        newPosition.z = -20;
+        if(target.position.x + halfWidth > boundMaxX) {
+            newPosition.x = boundMaxX - halfWidth;
+        }
+
+        if(target.position.x - halfWidth < boundMinX) {
+            newPosition.x = boundMinX + halfWidth;
+        }
+
+        if(target.position.y + halfHeight > boundMaxY) {
+            newPosition.y = boundMaxY - halfHeight;
+        }
+
+        if(target.position.y - halfHeight < boundMinY) {
+            newPosition.y = boundMinY + halfHeight;
+        }
+
+        transform.position = newPosition;
     }
 }
--- Assets/Enemies/Sombra1/Sombra1Controler.cs
+++ Assets/Enemies/Sombra1/Sombra1Controler.cs
@@ -9,22 +9,43 @@ public class SombraControler : MonoBehaviour
     private GameObject Player;
     private SpriteRenderer spr;
 
+    private bool attacking;
+    private Vector3 initialPosition;
+
     // Start is called before the first frame update
     void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player"); 
         spr = GetComponent<SpriteRenderer>();
+        initialPosition = transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")) {
+            attacking = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.CompareTag("Player")) {
+            attacking = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        var playerPos = Player.transform.position;
-
         float step = speed * Time.deltaTime;
-        transform.position = Vector2.MoveTowards(transform.position, playerPos, step);
+        
+        Vector3 targetPos = initialPosition;
 
-        if(playerPos.x < transform.position.x) {
+        if(attacking) {
+            targetPos = Player.transform.position;
+        };
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, step);
+
+        if(targetPos.x < transform.position.x) {
             spr.flipX = false;
         } else {
             spr.flipX = true;
