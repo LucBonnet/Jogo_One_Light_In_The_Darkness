9bf3e1cef4e5a1e833be1ad3c9c0f5904dc74306
LucBonnet <lucbonnet10@gmail.com>
2024-05-01 14:44:18 -0300

    personagens passando por objetos

--- Assets/Cenario/ObjetoCenarioControler.cs
+++ Assets/Cenario/ObjetoCenarioControler.cs
@@ -5,41 +5,47 @@ using UnityEngine;
 public class ObjetoCenarioControler : MonoBehaviour
 {
     private SpriteRenderer spr;
-    private CircleCollider2D cc2d;
-    private PolygonCollider2D pc2d;
+    private BoxCollider2D bc2d;
+    private Vector2 bc2dInitialOffset;
 
     // Start is called before the first frame update
     void Start()
     {
         spr = GetComponent<SpriteRenderer>();
-        cc2d = GetComponent<CircleCollider2D>();
-        pc2d = GetComponent<PolygonCollider2D>();
+        bc2d = GetComponent<BoxCollider2D>();
+
+        bc2dInitialOffset = bc2d.offset;
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(!other.gameObject.TryGetComponent(out SpriteRenderer sp)) return;
         
-        float otherY = sp.bounds.min.y;
+        float otherMinY = sp.bounds.min.y;
+        float otherMaxY = sp.bounds.max.y;
         float objectY = spr.bounds.min.y - 0.5f;
-        if(otherY > objectY) {
+        if(otherMinY > objectY) {
             // o player passa por tras
-            if(cc2d != null) {
-                cc2d.enabled = true;
-            }
-            if(pc2d != null) {
-                pc2d.enabled = true;
-            }
+            bc2d.enabled = true;
+            bc2d.offset = new Vector2(bc2dInitialOffset.x, bc2dInitialOffset.y);
             spr.sortingLayerName = "cima";
         } else {
             // o player passa pela frente
-            if(cc2d != null) {
-                cc2d.enabled = false;
-            }   
-            if(pc2d != null) {
-                pc2d.enabled = false;
+            if(otherMaxY < objectY) {
+                bc2d.enabled = false;
+                bc2d.offset = new Vector2(bc2dInitialOffset.x, bc2dInitialOffset.y);
+            } else {
+                float offset = otherMaxY - otherMinY - 0.2f;
+                bc2d.offset = new Vector2(bc2dInitialOffset.x, bc2dInitialOffset.y + offset);
+                bc2d.enabled = true;
             }
             spr.sortingLayerName = "player";
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        bc2d.enabled = false;
+        bc2d.offset = new Vector2(bc2dInitialOffset.x, bc2dInitialOffset.y);
+        spr.sortingLayerName = "player";
     }
 
     // Update is called once per frame
