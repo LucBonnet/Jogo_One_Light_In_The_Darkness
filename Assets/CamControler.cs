c482fc0e315a3714393ea99c0d894937921e5a15
Luc Bonnet <lucbonnet10@gmail.com>
2024-05-10 13:20:58 -0300

    correção colisão da camera

--- Assets/CamControler.cs
+++ Assets/CamControler.cs
@@ -10,15 +10,20 @@ public class CamControler : MonoBehaviour
     private float timerCam = 0.0f;
     private float waitTimeCam = 0.1f;
     private float radius;
+    private float colliderRadius;
     private Light2D luz;
     private int camType;
     private float damage;
+    private CircleCollider2D cc2d;
+
 
     // Start is called before the first frame update
     void Start()
     {
         luz = GetComponent<Light2D>();
+        cc2d = GetComponent<CircleCollider2D>();
         radius = luz.pointLightOuterRadius;
+        colliderRadius = cc2d.radius;
     }
 
     public void SetCamType(int value) {
@@ -44,10 +49,12 @@ public class CamControler : MonoBehaviour
             luz.pointLightOuterRadius = radius;
             luz.intensity = 1;
             damage = DAMAGE_CAM_1;
+            cc2d.radius = colliderRadius;
         } else if(camType == 2) {
             luz.pointLightOuterRadius = radius * 1.5f; 
             luz.intensity = 1;
             damage = DAMAGE_CAM_2;
+            cc2d.radius = colliderRadius * 1.5f;
         }
         timerCam += Time.deltaTime;
         if (timerCam >= waitTimeCam){
